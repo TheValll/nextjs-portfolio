b55f423a1f39491a20c45ec7bf3a29e84644f6f7
TheValll <valentin78.massonniere@gmail.com>
2023-11-01 13:30:22 +0100

    first page

--- app/components/Presentation.jsx
+++ app/components/Presentation.jsx
@@ -6,22 +6,52 @@ const Presentation = () => {
       <div className="left">
         <div className="title">
           <h1>
-            Full-Stack Junior <br /> Developper 📱
+            Full-Stack Junior <br /> Developer 📱
           </h1>
         </div>
         <div className="description">
           <p>
             Hi, I,m Valentin Massonnière. A passionate Full-Stack <br /> Junior
-            Developper based in LePecq, France &#x1f1eb;&#x1f1f7;
+            Developer based in LePecq, France 📍
           </p>
         </div>
         <div className="socials">
-          <a href="" target="_blank">
+          <a
+            href="https://www.linkedin.com/in/valentin-massonniere/"
+            target="_blank"
+          >
             <i className="fa-brands fa-linkedin"></i>
           </a>
-          <a href="" target="_blank">
+          <a href="https://github.com/TheValll" target="_blank">
             <i className="fa-brands fa-square-github"></i>
           </a>
+        </div>
+        <div className="tech">
+          <h3>Tech Stack |</h3>
+          <div className="icon">
+            <img src="/assets/icon/js.png" alt="js logo" height="40px" />
+            <img src="/assets/icon/ts.png" alt="ts logo" height="40px" />
+          </div>
+          <div className="icon">
+            <img src="/assets/icon/react.png" alt="react logo" height="40px" />
+            <img
+              src="/assets/icon/nextjs.png"
+              alt="nextjs logo"
+              height="40px"
+            />
+          </div>
+          <div className="icon">
+            <img src="/assets/icon/php.png" alt="php logo" height="40px" />
+            <img
+              src="/assets/icon/symfony.png"
+              alt="symfony logo"
+              height="40px"
+            />
+          </div>
+          <div className="icon">
+            <img src="/assets/icon/sass.png" alt="sass logo" height="40px" />
+            <img src="/assets/icon/git.png" alt="sass logo" height="40px" />
+          </div>
         </div>
       </div>
       <div
