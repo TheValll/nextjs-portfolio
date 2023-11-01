316b0720900a91319c8ed07848220a83c0da02ad
TheValll <valentin78.massonniere@gmail.com>
2023-11-01 21:45:39 +0100

    nestJS portfolio

--- app/page.jsx
+++ app/page.jsx
@@ -12,6 +12,7 @@ export default function Home() {
         rel="stylesheet"
         href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css"
       />
+      <link rel="icon" href="/assets/img/favicon.png" />
       <Navbar />
       <Presentation />
       <About />
